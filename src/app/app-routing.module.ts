924348ceb820dbe96a4a52470607cf1fc08796cb
Alejandro Delgadillo <68914180+alexdelgadillosan@users.noreply.github.com>
2023-06-22 19:35:19 -0600

    Fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
 import { PortfoliopageComponent } from './portfoliopage/portfoliopage.component';
-import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { HashLocationStrategy, LocationStrategy, PathLocationStrategy } from '@angular/common';
 
 
 const routes: Routes = [
@@ -14,7 +14,7 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
   providers: [
-    { provide: LocationStrategy, useClass: HashLocationStrategy }
+    { provide: LocationStrategy, useClass: PathLocationStrategy  }
   ],
 })
 export class AppRoutingModule { }
