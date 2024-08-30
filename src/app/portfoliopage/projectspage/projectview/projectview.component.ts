355c398c006acb2aa7843ad3481f4eb534a34077
Alejandro Delgadillo <68914180+alexdelgadillosan@users.noreply.github.com>
2024-08-29 22:09:41 -0600

    new updates

--- src/app/portfoliopage/projectspage/projectview/projectview.component.ts
+++ src/app/portfoliopage/projectspage/projectview/projectview.component.ts
@@ -3,12 +3,12 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-projectview',
   templateUrl: './projectview.component.html',
-  styleUrls: ['./projectview.component.css']
+  styleUrls: ['./projectview.component.css'],
 })
 export class ProjectviewComponent {
   @Input() imgSrc = '';
   @Input() title = '';
   @Input() description = '';
   @Input() shortDescription = '';
-  @Input() linkUrl: string = '#';
+  @Input() linkUrl: string = '';
 }
