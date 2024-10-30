ead6a438a09fc5c28d27cf81047a819eb823b847
Alejandro Delgadillo <68914180+alexdelgadillosan@users.noreply.github.com>
2024-10-30 13:04:41 -0600

    added counter

--- src/app/portfoliopage/contact/contact.component.ts
+++ src/app/portfoliopage/contact/contact.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as countapi from 'countapi-js';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.css'],
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
+  visitCount: number = 0;
 
+  ngOnInit() {
+    const namespace = 'alexdelgadillo';
+    const key = 'visit_counteralex';
+
+    // Primero obtenemos el conteo actual y lo mostramos
+    countapi.get(namespace, key).then((result: { value: number }) => {
+      this.visitCount = result.value;
+
+      // Luego incrementamos el conteo en el servidor
+      countapi.hit(namespace, key).then((newResult: { value: number }) => {
+        this.visitCount = newResult.value;
+      });
+    });
+  }
 }
